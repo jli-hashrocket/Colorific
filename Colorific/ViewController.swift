31b961a1e5cc91e921f5f4048b83167d4feeceb4
macheteshuffle <jeff.li029@gmail.com>
2017-01-02 13:58:56 -0500

    Colorific iOS app - create color rows, segue to color screen

--- Colorific/ViewController.swift
+++ Colorific/ViewController.swift
@@ -8,18 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var selectedColor: UIColor?
+    
+    let colors: [UIColor] = [UIColor.red, UIColor.blue, UIColor.green, UIColor.purple, UIColor(red: 255/255, green:0/255 ,blue: 120/255, alpha: 1.0), UIColor.black]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = self.colors[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.selectedColor = self.colors[indexPath.row]
+        self.performSegue(withIdentifier: "toColorScreen", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toColorScreen" {
+            segue.destination.view.backgroundColor = self.selectedColor
+        }
+    }
 
 }
 
